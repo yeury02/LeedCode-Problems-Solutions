d4947985ec4f1ecd6a1f2d7a95c149f5c8449a80
Yeury Galva Liriano <yeury02@my.uri.edu>
2019-12-04 19:08:45 -0500

    finish func

--- split_string_balanced_string.py
+++ split_string_balanced_string.py
@@ -7,21 +7,18 @@ Return the maximum amount of splitted balanced strings.
 
 class Solution:
     def balancedStringSplit(self, s):
-
         lcount = 0
         rcount = 0
         count = 0
-
         for i in range(len(s)):
-
             if s[i] == "R":
                 rcount += 1
             if s[i] == "L":
                 lcount += 1
             if lcount == rcount:
                 count += 1
-
         return count
+    
 s = "RLRRLLRLRL"
 object = Solution()
 print(object.balancedStringSplit(s))
