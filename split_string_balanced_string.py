ebe7dcd6123f3a9cdf3ca8bde20641c44abacb73
Yeury Galva Liriano <yeury02@my.uri.edu>
2019-12-04 19:05:35 -0500

    FInish function

--- split_string_balanced_string.py
+++ split_string_balanced_string.py
@@ -3,3 +3,22 @@ Balanced strings are those who have equal quantity of 'L' and 'R' characters.
 Given a balanced string s split it in the maximum amount of balanced strings.
 
 Return the maximum amount of splitted balanced strings.
+
+
+class Solution:
+    def balancedStringSplit(self, s):
+
+        lcount = 0
+        rcount = 0
+        count = 0
+
+        for i in range(len(s)):
+
+            if s[i] == "R":
+                rcount += 1
+            if s[i] == "L":
+                lcount += 1
+            if lcount == rcount:
+                count += 1
+
+        return count
