c4e7a7adce3f0d76103db43fb8f5a8665316ae04
Yeury Galva Liriano <yeury02@my.uri.edu>
2019-12-04 19:04:33 -0500

    Solved this function

---
+++ split_string_balanced_string.py
@@ -0,0 +1,5 @@
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
