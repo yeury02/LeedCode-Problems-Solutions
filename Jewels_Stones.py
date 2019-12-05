b0f1cfe2db2f5df5c28ad535519247fefa27e418
Yeury Galva Liriano <yeury02@my.uri.edu>
2019-12-05 16:22:56 -0500

    added comment

--- Jewels_Stones.py
+++ Jewels_Stones.py
@@ -6,6 +6,8 @@ You want to know how many of the stones you have are also jewels.
 The letters in J are guaranteed distinct, and all characters in J and S are letters.
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 '''
+# S and J will consist of letters and have length at most 50.
+# The characters in J are distinct.
 class Solution:
     def numJewelsInStones(self, J, S):
         count = 0
